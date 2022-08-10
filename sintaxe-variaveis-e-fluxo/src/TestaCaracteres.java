5c4b4d4b362556523f953586c5ebdd53d047800b
Carolina  Castro de Oliveira <carolina.oliveira@MacBook-Air-de-c0274.local>
2022-08-10 00:02:37 -0300

    primeiro commit

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,19 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		char letra = 'A';
+		System.out.println(letra);   // imprime A
+		
+		char valor = 66;
+		System.out.println(valor);   // imprime B
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);   // imprime C
+		
+		String palavra = "alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+	}
+
+}
